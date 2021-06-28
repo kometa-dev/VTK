6493cb82900d88cbfc4e93b158a46f3cdba226f9
Сергей Хлутчин <sercxjo@gmail.com>
2021-06-28 23:59:27 +0400

    Revert "Removing QVTKWidget2 and GUISupportQtOpenGL module"
    This reverts commit 91a053d786a71f812588815f7c13cb14413dfe1a.

--- Examples/GUI/Qt/GraphicsView/GraphicsView.hpp
+++ Examples/GUI/Qt/GraphicsView/GraphicsView.hpp
@@ -4,7 +4,7 @@
 
 #include <QGraphicsView>
 #include <QResizeEvent>
-#include "QVTKOpenGLNativeWidget.h"
+#include "QVTKWidget2.h"
 #include "OpenGLScene.hpp"
 #include "vtkGenericOpenGLRenderWindow.h"
 #include "vtkRenderer.h"
@@ -16,7 +16,7 @@ class GraphicsView : public QGraphicsView
     GraphicsView()
     {
       mCtx = new QGLContext(QGLFormat());
-      mWidget = new QVTKOpenGLNativeWidget(mCtx);
+      mWidget = new QVTKWidget2(mCtx);
       this->setViewport(mWidget);
       this->setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
       this->setScene(new OpenGLScene(mCtx, this));
@@ -57,7 +57,7 @@ class GraphicsView : public QGraphicsView
         mWidget->GetRenderWindow()->SetSize(event->size().width(), event->size().height());
       }
     QGLContext* mCtx;
-    QVTKOpenGLNativeWidget* mWidget;
+    QVTKWidget2* mWidget;
 };
 
 #endif
