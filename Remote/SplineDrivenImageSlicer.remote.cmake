717291ede707cb17f4516deb1f8b8984d09b4517
Сергей Хлутчин <sercxjo@gmail.com>
2021-12-24 14:43:33 +0400

    Switch SplineDrivenImageSlicer git repository to kometa-dev due to compile errors. QTVIEW-21

--- Remote/SplineDrivenImageSlicer.remote.cmake
+++ Remote/SplineDrivenImageSlicer.remote.cmake
@@ -4,6 +4,6 @@
 
 vtk_fetch_module(SplineDrivenImageSlicer
         "Spline Driven Image Slicer - http://www.vtkjournal.org/browse/publication/838"
-        GIT_REPOSITORY https://github.com/martinken/midas-journal-838.git
-        GIT_TAG 5a98940c705748df5c2470331541a051559956c8
+        GIT_REPOSITORY https://github.com/kometa-dev/midas-journal-838.git
+        GIT_TAG origin/master
         )
